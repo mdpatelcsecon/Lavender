59c1e8ac4c1a530d2e0d0042c7cfc2b093fa1a93
Mohit D Patel <mdpatel.csecon@outlook.com>
2024-12-30 14:00:37 -0500

    Started implementing the From<MemoryMapResponse> for PhysicalFrameAllocator to integrate it with Limine

--- nova/src/hal/environment/boot_protocol/mod.rs
+++ nova/src/hal/environment/boot_protocol/mod.rs
@@ -3,13 +3,12 @@
 //! This module exists to abstract the boot protocol used to boot the kernel. As of now and for the foreseeable future
 //! this kernel will only support the Limine boot protocol on top of UEFI or the subset of UEFI required by the EBBR
 //! standard. This is in line with the system requirements for ARM64 systems including compliance with the requirements
-//! of either the SystemReady or SystemReady DeviceTree bands of the ARM SystemReady program even if not formally
-//! certified.
+//! of ARM SystemReady program.
 //!
 //! Systems that do not meet the aforementioned requirements are not supported and will never be supported in line with
-//! the philosophy of this project to exclusively support and actively encourage standardized systems. Please do not
-//! open issues or PRs requesting or implementing support for non-standardized systems. They will be rejected. If you
-//! would like support on a given device please contact the manufacturer and request that they enable support for
-//! either the SystemReady or SystemReady DeviceTree band of the ARM SystemReady program.
+//! the philosophy of this project to exclusively support and actively encourage the proliferation of standardized
+//! systems. Please do not open issues or PRs requesting or implementing support for non-standardized systems. They will
+//! be rejected. If you would like support on a given device please contact the manufacturer and request that they
+//! provide support for ARM SystemReady compliant firmware.
 
 pub mod limine;
--- nova/src/memory/pmem/mod.rs
+++ nova/src/memory/pmem/mod.rs
@@ -1,11 +1,14 @@
 use lazy_static::lazy_static;
+use limine::response::MemoryMapResponse;
 
 use crate::hal::environment::boot_protocol::limine::HHDM_REQUEST;
+use crate::hal::isa::current_isa::memory::MemoryInterfaceImpl;
+use crate::hal::isa::interface::memory::address::PhysicalAddress;
 #[cfg(target_arch = "x86_64")]
 use crate::hal::isa::interface::memory::MemoryInterface;
-use crate::hal::isa::current_isa::memory::MemoryInterfaceImpl;
+use crate::hal::isa::x86_64::memory::address::paddr::PAddr;
 
-type VAddr = <MemoryInterfaceImpl as MemoryInterface>::VAddr;
+pub type VAddr = <MemoryInterfaceImpl as MemoryInterface>::VAddr;
 
 lazy_static! {
     pub static ref HHDM_BASE: VAddr = if let Some(response) = HHDM_REQUEST.get_response() {
@@ -13,4 +16,62 @@ lazy_static! {
     } else {
         panic!("Limine failed to provide a higher half direct mapping region.");
     };
+}
+#[derive(Debug)]
+pub enum Error {
+    UnableToAllocateTrackingStructure,
+}
+
+pub struct PhysicalFrameAllocator {
+    huge_page_map: &'static [u8],
+    large_page_map: &'static [u8],
+    page_map: &'static [u8],
+}
+
+impl PhysicalFrameAllocator {}
+
+// There should be a From implementation for each type of memory map we support.
+
+impl From<&limine::response::MemoryMapResponse> for PhysicalFrameAllocator {
+    fn from(response: &MemoryMapResponse) -> Self {
+        let bitmap_size = compute_bitmap_size(response);
+        let large_bitmap_size = bitmap_size / 512 + 1;
+        let huge_bitmap_size = large_bitmap_size / 512 + 1;
+
+        let bitmap_addr: PAddr = find_mmap_best_fit(response, bitmap_size).unwrap();
+        let large_bitmap_addr: PAddr = find_mmap_best_fit(response, large_bitmap_size).unwrap();
+        let huge_bitmap_addr: PAddr = find_mmap_best_fit(response, huge_bitmap_size).unwrap();
+
+        todo!()
+    }
+}
+
+fn compute_bitmap_size(mmap: &MemoryMapResponse) -> usize {
+    let mut highest_address: PAddr = PAddr::from(0);
+    // Find the highest address in the memory map.
+    for entry in mmap.entries().iter() {
+        let entry_end = entry.base + entry.length;
+        if entry_end > <PAddr as Into<usize>>::into(highest_address) as u64 {
+            highest_address = PAddr::from(entry_end as usize);
+        }
+    }
+
+    (<PAddr as Into<usize>>::into(highest_address) / 4096 + 1) / 8 + 1
+}
+
+fn find_mmap_best_fit(mmap: &MemoryMapResponse, size: usize) -> Result<PAddr, Error> {
+    let mut best_fit = PAddr::from(0);
+    let mut best_fit_size = 0;
+    for entry in mmap.entries().iter() {
+        let entry_size = entry.length;
+        if entry_size >= size as u64 && (best_fit_size == 0 || entry_size < best_fit_size) {
+            best_fit = PAddr::from(entry.base as usize);
+            best_fit_size = entry_size;
+        }
+    }
+    if best_fit == PAddr::from(0) {
+        Err(Error::UnableToAllocateTrackingStructure)
+    } else {
+        Ok(best_fit)
+    }
 }
