474b7f604d339269f78687f1039df30e20850310
Mohit Patel <mdpatel.csecon@outlook.com>
2024-12-31 13:15:33 -0500

    Implement the beginnings of the PhysicalFrameAllocator with bitmap management and memory initialization

--- nova/src/memory/pmem/mod.rs
+++ nova/src/memory/pmem/mod.rs
@@ -1,3 +1,9 @@
+//! # Physical Memory Management
+//! 
+//! This module is responsible for managing physical memory. It provides an interface for allocating and freeing physical memory frames.
+
+use core::ptr::NonNull;
+
 use lazy_static::lazy_static;
 use limine::response::MemoryMapResponse;
 
@@ -7,6 +13,7 @@ use crate::hal::isa::interface::memory::address::PhysicalAddress;
 #[cfg(target_arch = "x86_64")]
 use crate::hal::isa::interface::memory::MemoryInterface;
 use crate::hal::isa::x86_64::memory::address::paddr::PAddr;
+use crate::logln;
 
 pub type VAddr = <MemoryInterfaceImpl as MemoryInterface>::VAddr;
 
@@ -20,29 +27,39 @@ lazy_static! {
 #[derive(Debug)]
 pub enum Error {
     UnableToAllocateTrackingStructure,
+    MisalignedPhysicalAddress,
 }
 
+
+
 pub struct PhysicalFrameAllocator {
-    huge_page_map: &'static [u8],
-    large_page_map: &'static [u8],
-    page_map: &'static [u8],
+    bitmap: &'static mut [u8],
 }
 
 impl PhysicalFrameAllocator {}
 
 // There should be a From implementation for each type of memory map we support.
 
-impl From<&limine::response::MemoryMapResponse> for PhysicalFrameAllocator {
+impl From<&MemoryMapResponse> for PhysicalFrameAllocator {
     fn from(response: &MemoryMapResponse) -> Self {
         let bitmap_size = compute_bitmap_size(response);
-        let large_bitmap_size = bitmap_size / 512 + 1;
-        let huge_bitmap_size = large_bitmap_size / 512 + 1;
+        logln!("PhysicalFrameAllocator bitmap size: {:?}", bitmap_size);
 
         let bitmap_addr: PAddr = find_mmap_best_fit(response, bitmap_size).unwrap();
-        let large_bitmap_addr: PAddr = find_mmap_best_fit(response, large_bitmap_size).unwrap();
-        let huge_bitmap_addr: PAddr = find_mmap_best_fit(response, huge_bitmap_size).unwrap();
+        logln!("PhysicalFrameAllocator bitmap addr: {:?}", bitmap_addr);
+        let pfa = PhysicalFrameAllocator {
+            bitmap: unsafe {
+                core::slice::from_raw_parts_mut(
+                    <PAddr as Into<*mut u8>>::into(bitmap_addr),
+                    bitmap_size,
+                )
+            },
+        };
+        // Initially mark all frames as unavailable.
+        pfa.bitmap.fill(1u8);
+        init_bitmap_from_mmap(pfa.bitmap, response);
 
-        todo!()
+        pfa
     }
 }
 
@@ -73,5 +90,31 @@ fn find_mmap_best_fit(mmap: &MemoryMapResponse, size: usize) -> Result<PAddr, Er
         Err(Error::UnableToAllocateTrackingStructure)
     } else {
         Ok(best_fit)
+    }
+}
+
+fn addr_to_bitmap_index(addr: PAddr) -> Result<(usize, usize), Error> {
+    if <PAddr as Into<usize>>::into(addr) % 4096 != 0 {
+        return Err(Error::MisalignedPhysicalAddress);
+    }
+
+    let bit_index = <PAddr as Into<usize>>::into(addr) / 4096;
+
+    let byte_index = bit_index / 8;
+    let bit_offset = bit_index % 8;
+
+    Ok((byte_index, bit_offset))
+}
+
+fn init_bitmap_from_mmap(bitmap: &mut [u8], mmap: &MemoryMapResponse) {
+    for entry in mmap.entries().iter() {
+        let start = entry.base;
+        let end = entry.base + entry.length;
+        let start_index = start / 4096;
+        let end_index = end / 4096;
+        for i in (start_index..end_index).step_by(4096) {
+            let (byte_index, bit_offset) = addr_to_bitmap_index(PAddr::from(i as usize)).unwrap();
+            bitmap[byte_index] &= 0 << bit_offset;
+        }
     }
 }
